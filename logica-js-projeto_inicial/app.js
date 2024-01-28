5c4695a866d267aff43ede2ffc9efa216682e667
thaleseqb <thalesquadros2@gmail.com>
2024-01-28 13:22:40 -0300

    adicionar a repetição enquanto condição não satisfeita e arquivo gitignore

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,15 +2,23 @@ alert("Jogo do número secreto está a sua frente");
 let secret_number = 8;
 
 console.log(secret_number)
-let value = prompt("Escolha um número entre 1 e 30");
+let value
 
-if (secret_number == value) {
-  alert(`número secreto encontrado, ${secret_number}`);
-} else  {
-  if (value>secret_number) {
-    alert(`O numero secreto é menor que ${value}`);
+// Enquanto o numero secreto não for igual ao valor selecionado 
+// pelo jogador é necessário fornecer outro numero
 
-  } else {
-    alert(`O numero secreto é maior que ${value}`)
+while (value != secret_number){
+  value = prompt("Escolha um número entre 1 e 30");
+  
+  if (secret_number == value) {
+    alert(`número secreto encontrado, ${secret_number}`);
+  } else  {
+    if (value>secret_number) {
+      alert(`O numero secreto é menor que ${value}`);
+  
+    } else {
+      alert(`O numero secreto é maior que ${value}`)
+    }
   }
 }
+
