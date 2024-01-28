49b108b0c6e368db9e7a60d11e536ec74ebf1a52
thaleseqb <thalesquadros2@gmail.com>
2024-01-28 15:05:12 -0300

    adicionar break e distinguir uma palavra em uma estrutura de decisão

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,24 +1,31 @@
 alert("Jogo do número secreto está a sua frente");
 let secret_number = 8;
 
-console.log(secret_number)
-let value
+console.log(secret_number);
+let value;
+let c = 1
 
 // Enquanto o numero secreto não for igual ao valor selecionado 
 // pelo jogador é necessário fornecer outro numero
 
-while (value != secret_number){
+while (value != secret_number) {
   value = prompt("Escolha um número entre 1 e 30");
   
   if (secret_number == value) {
-    alert(`número secreto encontrado, ${secret_number}`);
+    break;
   } else  {
     if (value>secret_number) {
       alert(`O numero secreto é menor que ${value}`);
   
     } else {
-      alert(`O numero secreto é maior que ${value}`)
+      alert(`O numero secreto é maior que ${value}`);
     }
+    c++
   }
 }
 
+if (c == 1) {
+  alert(`número secreto encontrado, ${secret_number}. Depois de ${c} tentativa`);
+} else {
+  alert(`número secreto encontrado, ${secret_number}. Depois de ${c} tentativas`);
+}
